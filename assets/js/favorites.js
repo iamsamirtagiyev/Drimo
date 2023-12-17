10f03085bdf0a04423294f4602855c3558ee913b
iamsamirtagiyev <stagiyev579@gmail.com>
2023-12-17 17:56:59 +0400

    drimo

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,7 +1,25 @@
-const cardList = document.querySelector('.card-list')
 
 axios.get('http://localhost:3000/favorites/').then(response => {
     response.data.forEach(item => {
-        dataContent(item.id, item.image, item.name, item.description)
+
+        cardList.innerHTML += `
+    <div class="card">
+        <div class="icon" onclick="goToDetails(${item.id})">
+        <img src=${item.image}>
+        </div>
+        <div class="info" onclick="goToDetails(${item.id})">
+            <h4>${item.name}</h4>
+            <p>${item.description}</p>
+        </div>
+        <div class="buttons">
+            <button onclick="deleteData(${item.id})">Delete</button>
+            <button onclick="updateData(${item.id})">Update</button>
+        </div>
+        <button onclick="delFav(${item.id})">Delete Favotites</button>
+    </div>`
     });
 })
+
+const delFav = (id) => {
+    axios.delete('http://localhost:3000/favorites/'+id).then(response => window.location.reload())
+}
